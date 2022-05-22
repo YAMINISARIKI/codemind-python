2965b1664c2f0d4afba6da182b931bfe5f6adbfd
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-05-22 21:10:18 +0530

    t=int(input()) for i in range(t):     a,b=map(int,input().split())     c=0     for i in range(0,b):         if i*i%b==a:             c=1             print(i)             break     if c==0:         print(-1)

---
+++ Monk_and_Square_Root.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    a,b=map(int,input().split())
+    c=0
+    for i in range(0,b):
+        if i*i%b==a:
+            c=1
+            print(i)
+            break
+    if c==0:
+        print(-1)
+            
