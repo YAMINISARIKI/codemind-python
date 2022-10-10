164bb6dffffce0bf2b131feba3665aed79853263
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-10-10 17:36:30 +0530

    n=input() m=input() n=n.lower() m=m.lower() c=0 c1=0 for i in n:     c1+=1     if i in m:         c+=1 if(c==c1):     print("True") else:     print("False")

--- Anagrams.py
+++ Anagrams.py
@@ -1,16 +1,14 @@
-a=input()
-b=input()
-f=0
-if len(a)==len(b):
-    for i in a:
-        if i in b or i.upper() in b or i.lower() in b:
-            f=1
-        else:
-            f=0
-            break
-    if f==1:
-        print("True")
-    else:
-        print("False")
+n=input()
+m=input()
+n=n.lower()
+m=m.lower()
+c=0
+c1=0
+for i in n:
+    c1+=1
+    if i in m:
+        c+=1
+if(c==c1):
+    print("True")
 else:
     print("False")
