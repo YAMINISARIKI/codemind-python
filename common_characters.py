8d947267416f890160087dbba1e41fee8446b197
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-12-03 07:31:01 +0530

    s1=input().lower() s2=input().lower() a="" for i in s1:     if i in s2 and i not in a:         a+=i for i in s2:     if i in s1 and i not in a:         a+=i a=sorted(a) if len(a)==0:     print("-1") else:     for i in a:         if i!=" ":             print(i,end="")

---
+++ common_characters.py
@@ -0,0 +1,17 @@
+s1=input().lower()
+s2=input().lower()
+a=""
+for i in s1:
+    if i in s2 and i not in a:
+        a+=i
+for i in s2:
+    if i in s1 and i not in a:
+        a+=i
+a=sorted(a)
+if len(a)==0:
+    print("-1")
+else:
+    for i in a:
+        if i!=" ":
+            print(i,end="")
+        
