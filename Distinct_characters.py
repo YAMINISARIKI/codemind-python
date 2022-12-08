db5d899ef473739239f3a64bb2a6af48dacaadde
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-12-08 22:02:01 +0530

    n=input() n=n.lower() n=set(n) s="" for i in n:     if i!=" ":         s=s+i s=sorted(s) for i in s:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,11 +1,11 @@
-a=input()
-a=a.lower()
-a=set(a)
-b=[]
-for i in a:
+n=input()
+n=n.lower()
+n=set(n)
+s=""
+for i in n:
     if i!=" ":
-        x=ord(i)
-        b.append(x)
-b=set(b)
-for i in b:
-    print(chr(i),end="")
+        s=s+i
+s=sorted(s)
+for i in s:
+    print(i,end="")
+            
